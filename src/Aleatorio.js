adfd3971953619f357d6f65a250f358e1ba23b31
LucasATS <64665422+LucasATS@users.noreply.github.com>
2022-10-23 23:30:46 -0400

    Update Aleatorio.js

--- src/Aleatorio.js
+++ src/Aleatorio.js
@@ -2,7 +2,7 @@ function Aleatorio(de, ate, quant) {
   let results = [];
 
   for (let index = 0; index <= quant; index++) {
-    let valor = Math.floor((Math.random() * (ate - de)) + de);
+    let valor = Math.floor(Math.random() * (ate - de) + de);
     results.push(valor);
   }
 
